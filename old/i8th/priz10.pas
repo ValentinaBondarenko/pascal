17f38c1c4fe0bf979c96074a5dbd8da2c9583683
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2019-09-29 17:10:32 +0300

    firt file. Do not do commit messages like this

---
+++ old/i8th/priz10.pas
@@ -0,0 +1,20 @@
+﻿program priz10;
+uses graphabc;
+var x,y,r: integer;
+begin
+
+x:=22;
+y:=458;
+r:=20;
+while x<600  do
+begin
+clearwindow(clwhite);
+setbrushcolor(clblue);
+circle(x,y,r);
+sleep(10);
+setbrushcolor(clwhite);
+setpencolor(clwhite);
+circle(x,y,r);
+x:=x+4;
+end;
+end.
