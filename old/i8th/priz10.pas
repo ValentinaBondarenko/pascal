28143ab0717a8bdbfbda2372448112f6ccfaa04b
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2019-09-29 17:15:21 +0300

    Formatted file

--- old/i8th/priz10.pas
+++ old/i8th/priz10.pas
@@ -2,19 +2,18 @@
 uses graphabc;
 var x,y,r: integer;
 begin
-
-x:=22;
-y:=458;
-r:=20;
-while x<600  do
-begin
-clearwindow(clwhite);
-setbrushcolor(clblue);
-circle(x,y,r);
-sleep(10);
-setbrushcolor(clwhite);
-setpencolor(clwhite);
-circle(x,y,r);
-x:=x+4;
-end;
+  x:=22;
+  y:=458;
+  r:=20;
+  while x<600 do
+  begin
+    clearwindow(clwhite);
+    setbrushcolor(clblue);
+    circle(x,y,r);
+    sleep(10);
+    setbrushcolor(clwhite);
+    setpencolor(clwhite);
+    circle(x,y,r);
+    x:=x+4;
+  end;
 end.
