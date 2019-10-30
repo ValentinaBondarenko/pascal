e0701816bcecb5bab75a5b5e87d6697ca91003c6
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2019-10-30 23:32:06 +0300

    i've finished 9th class

---
+++ old/i9th/p10th/primer10_1a.pas
@@ -0,0 +1,33 @@
+﻿program primer10_1a;
+Var R,V: array[1..6]  of integer; 
+  i,h,d:integer;
+  g,p,m,s,msum: real;
+begin
+writeln('Введите радиусы сосудов через пробел');
+for i:=1 to 6 do
+read(R[i]);
+writeln('Введите высоту столба жидкости через пробел');
+ for i:=1 to 6 do
+ read(V[i]);
+ g:=9.8;
+ d:=1200;
+ for i:=1 to 6 do
+ begin
+ p:=d*g*V[i];
+ writeln('давление столба жидкости ',i,' сосуда равно ', p);
+ end;
+ writeln;
+ msum:=0;
+ for i:=1 to 6 do 
+ begin
+ s:=3.14*sqr(R[i]);
+ m:=d*V[i]*s;
+ writeln('масса жидкости ',i,'сосуда равна ', m);
+ msum:=msum+m;
+ end;
+ writeln;
+ write('суммарная масса жидкости равна ', msum);
+end.
+
+
+
---
+++ old/i9th/p10th/primer10_2.pas
@@ -0,0 +1,20 @@
+﻿program primer10_2;
+Var T: array[1..6]  of integer; 
+    V: array[1..6] of real;
+    i,q:integer;
+  m: real;
+begin
+writeln('Введите температуру');
+for i:=1 to 6 do
+read(T[i]);
+q:=420;
+m:=0.12;
+for i:=1 to 6 do
+begin
+V[i]:=q/(m*T[i]);
+writeln('теплоемкость вещества ',i, ' образца ', V[i]:8:2);
+end;
+end.
+
+
+
