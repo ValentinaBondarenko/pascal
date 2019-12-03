f9cbf3bdeed06d44850c723d5c420c23a19c72f8
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2019-12-03 22:46:59 +0300

    two tasks i have done

---
+++ old/i11th/p7th/primer7_2_1a.pas
@@ -0,0 +1,15 @@
+﻿program primer7_2_2a;
+uses  GraphABC;
+Var pic : picture;
+    
+begin
+pic:=picture.create('D:/Pascal/photo/enshtein.jpg');
+pic.Draw(10,10,150,200);
+pic.Load('D:/Pascal/photo/Rezerford.jpg');
+ pic.Draw(170,10,150,200);
+pic.Load('D:/Pascal/photo/bor.jpg');
+pic.Draw(330,10,150,200);
+end.
+
+//это  портреты ученых//
+
