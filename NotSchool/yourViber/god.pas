dc3d22f1e89d70aa786d9d9c60e366eb9fe0b4e6
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2019-12-11 23:38:55 +0300

    i have done 2 nasks from viber

--- NotSchool/yourViber/god.pas
+++ NotSchool/yourViber/god.pas
@@ -4,9 +4,10 @@ var a: integer;
 begin
   write('Введите число ');
  read(a);
- if a mod 4=0 then
- write('високосный год ')
- else write('обычный год ');
+ if (a mod 4 <>0) or (a mod 100=0) and (a mod 400<>0) then
+ 
+ writeln('обычный год ')
+ else write('високосный год ');
 writeln(a);
  
 
---
+++ NotSchool/yourViber/kvadr_ur.pas
@@ -0,0 +1,26 @@
+﻿program kvadr_ur;
+var a,b,c:real;
+    x1,x2,d: real;
+
+begin
+  write('Введите коэффициенты a,b,c ');
+ read(a,b,c);
+ d:=b*b-4*a*c;
+ if d<0  then 
+ writeln('Корней нет ')
+ else if d=0 then
+ begin
+ x1:=-b/2/a;
+ write('x1=x2=',x1);
+end
+else begin
+x1:=(-b-sqrt(d))/2/a;
+x2:=(-b+sqrt(d))/2/a;
+writeln('x1= ',x1,' x2= ',x2);
+end;
+ 
+
+end.
+
+
+//корни квадратного уравнения//
