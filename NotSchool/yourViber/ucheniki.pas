54256cc23950a6234799911d717dfb2e687bab1b
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2019-12-14 23:17:36 +0300

    finished one of your tasks

---
+++ NotSchool/yourViber/ucheniki.pas
@@ -0,0 +1,14 @@
+﻿program ucheniki;
+Const R: array[1..15] of integer = (158,162,174,177,181,169,
+158,165,168,172,169,158,174,153,176);
+var a,b,i:integer;
+    
+begin
+b:=R[5];
+ a:=0;
+ for i:=1 to 15 do
+  if R[i]
+end.
+
+
+//недоделанный массив//
