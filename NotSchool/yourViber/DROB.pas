4ca41efc791806d47162294f33b4481e03f4e3a3
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2020-01-06 22:09:19 +0300

    drob is done

---
+++ NotSchool/yourViber/DROB.pas
@@ -0,0 +1,21 @@
+﻿Program DROB;
+uses crt;
+var
+a, b, c, d: integer;
+s: real;
+Begin
+Write('Введите числитель a1 = ');
+Readln(a);
+Write('Введите знаменатель b1 = ');
+Readln(b);
+Writeln;
+Write('Введите числитель с2 = ');
+Readln(c);
+Write('Введите знаменатель d2 = ');
+Readln(d);
+S:=(a*d+b*c)/(b*d);
+Writeln;
+write('Сумма дробей s = ', s:0:3);
+readln;
+end.
+
