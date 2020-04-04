e44f4becde0f17095223b8bcc6c325ebeb4c5393
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2020-04-04 11:28:28 +0300

    added formating and a bit of structure

--- NotSchool/yourViber/CommonFraction.pas
+++ NotSchool/yourViber/CommonFraction.pas
@@ -1,21 +1,29 @@
 ﻿Program CommonFraction;
 var
-a, b, c, d: integer;
-fractionOperator : string;
-s: real;
+  a, b, c, d: integer;
+  fractionOperator : string;
+  s: real;
 Begin
-Write('Введите числитель a1 = ');
-Readln(a);
-Write('Введите знаменатель b1 = ');
-Readln(b);
-Writeln;
-Write('Введите числитель с2 = ');
-Readln(c);
-Write('Введите знаменатель d2 = ');
-Readln(d);
-Write('Введите оператор (+,-,*,/)');
-Readln(fractionOperator);
-S:=(a*d+b*c)/(b*d);
-Writeln;
-writeln('Сумма дробей s = ', (a*d+b*c),'/',(b*d));
+  // Data input
+  Write('Введите числитель a1 = ');
+  Readln(a);
+  
+  Write('Введите знаменатель b1 = ');
+  Readln(b);
+  
+  Write('Введите числитель с2 = ');
+  Readln(c);
+  
+  Write('Введите знаменатель d2 = ');
+  Readln(d);
+  
+  Write('Введите оператор (+,-,*,/)');
+  Readln(fractionOperator);
+  
+  // Calculation
+  S:=(a*d+b*c)/(b*d);
+  
+  // Output
+  Writeln;
+  writeln('Сумма дробей s = ', (a*d+b*c),'/',(b*d));
 end.
