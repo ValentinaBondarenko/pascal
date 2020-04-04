1dcc3d04aceb561c35e54ce640aeeac364356163
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2020-04-04 10:10:25 +0300

    programm that determines if file is graphical one

---
+++ NotSchool/yourViber/files.pas
@@ -0,0 +1,19 @@
+﻿Program files;
+
+Var 
+  s,n,a,b,c,d,m,p: string;                  
+i,z,y: integer;
+
+Begin
+m:='jpg';
+p:='png';
+writeln('введите название файла');
+readln(a,b,c,d,n,s);
+
+//delete(слово,позиция, позиция и длина )//
+//len:= length(a,b,c,d,n,s) любой строки//
+writeln('Это графический файл')
+else ('это другой файл');
+
+end.
+
