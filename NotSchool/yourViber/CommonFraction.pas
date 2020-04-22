f0566bf4d034a472b2c67f9038d30387d9988ed3
Valentina Bondarenko <valentina.bondarenko.code@gmail.com>
2020-04-22 08:27:21 +0300

    1. Corrected boolean logic (added AND compound condition). 2. Instead of many if statements we introduced array of prime numbers and iterated over it.

--- NotSchool/yourViber/CommonFraction.pas
+++ NotSchool/yourViber/CommonFraction.pas
@@ -1,5 +1,8 @@
 ﻿Program CommonFraction;
 var
+  i : integer;
+  arraySize : integer;
+
   oneNumerator : integer;
   oneDenominator : integer;
   twoNumerator : integer;
@@ -10,35 +13,43 @@ var
   resultString : string;
   
   fractionOperator : string; // 'operator' is the reserved word
+  
+  Const thePrimeNumbers: array[1..58] of integer = (
+    2, 3,  5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 
+    71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 
+    151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 
+    233, 239, 241, 251, 257, 263, 269, 271
+  );
+  
 Begin
   // Data input
-  Write('Please enter the first fraction numerator = ');
-  Readln(oneNumerator);
-  
-  Write('Please enter the first fraction denominator = ');
-  Readln(oneDenominator);
-  
-  Write('Please enter the second fraction numerator = ');
-  Readln(twoNumerator);
-  
-  Write('Please enter the second fraction denominator = ');
-  Readln(twoDenominator);
-  
-  Write('Please enter operator (+,-,*,/)');
-  Readln(fractionOperator);
+      Write('Please enter the first fraction numerator = ');
+      Readln(oneNumerator);
+      
+      Write('Please enter the first fraction denominator = ');
+      Readln(oneDenominator);
+      
+      Write('Please enter the second fraction numerator = ');
+      Readln(twoNumerator);
+      
+      Write('Please enter the second fraction denominator = ');
+      Readln(twoDenominator);
+      
+      Write('Please enter operator (+,-,*,/)');
+      Readln(fractionOperator);
   
   // Calculation
     
   If fractionOperator = '+' then
     begin
-      resultNumerator := oneNumerator*twoDenominator+oneDenominator*twoNumerator;
+      resultNumerator := (oneNumerator*twoDenominator)+(oneDenominator*twoNumerator);
       resultDenominator := oneDenominator*twoDenominator;
       resultString := 'Sum';
     end
   else 
     if fractionOperator = '-' then
       begin
-        resultNumerator := oneNumerator*twoDenominator-oneDenominator*twoNumerator;
+        resultNumerator := (oneNumerator*twoDenominator)-(oneDenominator*twoNumerator);
         resultDenominator := oneDenominator*twoDenominator;
         resultString := '-';
       end      
@@ -59,67 +70,22 @@ Begin
   
   // Socraschenie
   
-   if (resultNumerator mod 9 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 9;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 9= 0) then  
-         abbreviatedDenominator:=resultDenominator div 9;
-         writeln (abbreviatedDenominator);
+  // Data from https://oeis.org/A000040
+ 
+    arraySize := 58;
   
-   if (resultNumerator mod 8 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 8;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 8= 0) then  
-         abbreviatedDenominator:=resultDenominator div 8;
-         writeln (abbreviatedDenominator);
-    
-     if (resultNumerator mod 7 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 7;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 7= 0) then  
-         abbreviatedDenominator:=resultDenominator div 7;
-         writeln (abbreviatedDenominator);
-    
-    if (resultNumerator mod 6 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 6;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 6= 0) then  
-         abbreviatedDenominator:=resultDenominator div 6;
-         writeln (abbreviatedDenominator);
-         
-  
-   if (resultNumerator mod 5 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 5;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 5= 0) then  
-         abbreviatedDenominator:=resultDenominator div 5;
-         writeln (abbreviatedDenominator);
-  
-  if (resultNumerator mod 4 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 4;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 4= 0) then  
-         abbreviatedDenominator:=resultDenominator div 4;
-         writeln (abbreviatedDenominator);
-         
-         if (resultNumerator mod 3 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 3;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 3= 0) then  
-         abbreviatedDenominator:=resultDenominator div 3;
-         writeln (abbreviatedDenominator);
-         
-         if (resultNumerator mod 2 = 0) then   
-        abbreviatedNumerator:=resultNumerator div 2;
-        writeln (abbreviatedNumerator);
-     if (resultDenominator mod 2= 0) then  
-         abbreviatedDenominator:=resultDenominator div 2;
-         writeln (abbreviatedDenominator);
+   for i:=1 to arraySize do
+   begin
+      if (resultNumerator mod thePrimeNumbers[i] = 0) and (resultDenominator mod thePrimeNumbers[i] = 0) then
+            resultNumerator:= (resultNumerator) div (thePrimeNumbers[i]);
+            resultDenominator:= (resultDenominator) div (thePrimeNumbers[i]);
+   end;
+     
   // Determinging of the whole (celyi) part   
       
   //output
   Writeln;
   
   resultString := resultString +  ' of fractions = ' + resultNumerator + '/' + resultDenominator;
-  
+  writeln(resultString);
 end.
